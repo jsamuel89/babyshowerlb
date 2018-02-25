f7d3c62e2849577d54fad63895e36827d7abe4ff
jsamuel89 <mailtosamuelj@gmail.com>
2018-02-26 00:26:55 +0530

    Bug fix

--- server/server.js
+++ server/server.js
@@ -4,34 +4,9 @@ var loopback = require('loopback');
 var boot = require('loopback-boot');
 var app = module.exports = loopback();
 
-app.start = function() {
-  // start the web server
-
-  var port = process.env.PORT || 3000;
-
-  // app.use(loopback.static(path.resolve(__dirname, '../client')));
-  // app.use(loopback.static(path.resolve(__dirname, '../.tmp')));
-
-  return app.listen(port, function() {
-    app.emit('started');
-    console.log('Web server listening at: %s', app.get('url'));
-  });
-
-/*
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
-*/
-
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
+
 boot(app, __dirname, function(err) {
   if (err) throw err;
 
@@ -39,3 +14,14 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+app.start = function() {
+  // start the web server
+
+  var port = process.env.PORT || 3000;
+
+  return app.listen(port, function() {
+    app.emit('started');
+    console.log('Web server listening at: %s', app.get('url'));
+  });
+};
